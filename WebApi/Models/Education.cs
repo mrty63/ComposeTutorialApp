cb53c46463e1601b22d9bef063df2c3ab4037210
Terry Namboya <terry.namboya@go4schools.com>
2022-05-18 13:01:33 +0100

    add class fiels for new featres

---
+++ WebApi/Models/Education.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Education
+    {
+		public string m_title { get; set; }
+		public string m_school { get; set; }
+		public DateTime m_start { get; set; }
+		public DateTime m_end { get; set; }
+		public string m_grade { get; set; }
+	}
+}
---
+++ WebApi/Models/Job.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Job
+    {
+        public string m_title { get; set; }
+        public string m_company { get; set; }
+        public DateTime m_start { get; set; }
+        public DateTime? m_end { get; set; }
+    }
+}
