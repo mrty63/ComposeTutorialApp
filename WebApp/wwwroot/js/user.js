fc0e7414b7220c6f36413e7a6e0b1f2d27338d64
Terry Namboya <terry.namboya@go4schools.com>
2022-06-09 20:00:09 +0100

    changed start page styleing added phototgraphy

---
+++ WebApp/wwwroot/js/user.js
@@ -0,0 +1,15 @@
+﻿
+function StyleIndex() {
+
+    console.log("hello world");
+    document.body.style.backgroundImage = "url(images/train.jpg)";
+    //alterFooter();
+}
+
+function alterFooter() {
+    const el = document.getElementById("footer");
+    console.log(el)
+    el.classList.add("HighlightWhiteText");
+
+}
+
